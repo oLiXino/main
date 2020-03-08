e78e0e69e5305181cc3da93afd4e0ef56475c293
amrl <amirul.ardy@gmail.com>
2020-03-08 19:03:51 +0800

    Initial Deck implementation

--- src/main/java/seedu/address/logic/commands/cardcommands/AddCardCommand.java
+++ src/main/java/seedu/address/logic/commands/cardcommands/AddCardCommand.java
@@ -7,15 +7,17 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.deck.Deck;
+import seedu.address.model.deck.card.Card;
 
 /**
- * Adds a person to the address book.
+ * Adds a card to the deck.
  */
 public class AddCardCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a card to the current deck. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds a card to the current deck. "
             + "Parameters: "
             + "FRONT "
             + ": "
@@ -26,27 +28,31 @@ public class AddCardCommand extends Command {
             + "thanks";
 
     public static final String MESSAGE_SUCCESS = "New card added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This card already exists in the deck";
+    public static final String MESSAGE_DUPLICATE_CARD = "This card already exists in the deck";
 
-    private final Deck toAdd;
+    private final Card toAdd;
 
     /**
-     * Creates an AddCommand to add the specified {@code Person}
+     * Creates an AddCommand to add the specified {@code Card}
      */
-    public AddCardCommand(Deck deck) {
-        requireNonNull(deck);
-        toAdd = deck;
+    public AddCardCommand(Card card) {
+        requireNonNull(card);
+        toAdd = card;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        
+        // model needs to know current deck
+        // then impl hasCard() method
+//        if (model.hasCard(toAdd)) {
+//            throw new CommandException(MESSAGE_DUPLICATE_CARD);
+//        }
 
-        if (model.hasPerson(toAdd)) {
-            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
-        }
-
-        model.addPerson(toAdd);
+        // model needs to know current deck
+        // then impl addCard() method
+//        model.addCard(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
--- src/main/java/seedu/address/model/deck/Deck.java
+++ src/main/java/seedu/address/model/deck/Deck.java
@@ -15,62 +15,41 @@ import seedu.address.model.deck.dump.Phone;
 import seedu.address.model.deck.dump.tag.Tag;
 
 /**
- * Represents a Person in the address book.
+ * Represents a Deck in the library.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Deck {
-
-    // New fields
-    private final UniqueCardList cards = new UniqueCardList();
-
+    
+    // todo: what other fields represent unique identity of a deck?
     // Identity fields
     private final Name name;
-    private final Phone phone;
-    private final Email email;
 
     // Data fields
-    private final Address address;
-    private final Set<Tag> tags = new HashSet<>();
+    private final UniqueCardList cards = new UniqueCardList();
 
     /**
      * Every field must be present and not null.
      */
-    public Deck(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Deck(Name name) {
+        requireAllNonNull(name);
         this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.tags.addAll(tags);
     }
 
     public Name getName() {
         return name;
     }
 
-    public Phone getPhone() {
-        return phone;
-    }
-
-    public Email getEmail() {
-        return email;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
+//    /**
+//     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+//     * if modification is attempted.
+//     */
+//    public Set<Tag> getTags() {
+//        return Collections.unmodifiableSet(tags);
+//    }
 
     /**
-     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
-     * if modification is attempted.
-     */
-    public Set<Tag> getTags() {
-        return Collections.unmodifiableSet(tags);
-    }
-
-    /**
-     * Returns true if both persons of the same name have at least one other identity field that is the same.
-     * This defines a weaker notion of equality between two persons.
+     * Returns true if both decks have the same name.
+     * This defines a weaker notion of equality between two decks.
      */
     public boolean isSameDeck(Deck otherDeck) {
         if (otherDeck == this) {
@@ -78,13 +57,12 @@ public class Deck {
         }
 
         return otherDeck != null
-                && otherDeck.getName().equals(getName())
-                && (otherDeck.getPhone().equals(getPhone()) || otherDeck.getEmail().equals(getEmail()));
+                && otherDeck.getName().equals(getName());
     }
 
     /**
-     * Returns true if both persons have the same identity and data fields.
-     * This defines a stronger notion of equality between two persons.
+     * Returns true if both decks have the same identity and data fields.
+     * This defines a stronger notion of equality between two decks.
      */
     @Override
     public boolean equals(Object other) {
@@ -97,32 +75,28 @@ public class Deck {
         }
 
         Deck otherDeck = (Deck) other;
-        return otherDeck.getName().equals(getName())
-                && otherDeck.getPhone().equals(getPhone())
-                && otherDeck.getEmail().equals(getEmail())
-                && otherDeck.getAddress().equals(getAddress())
-                && otherDeck.getTags().equals(getTags());
+        return otherDeck.getName().equals(getName());
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name);
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append(getName())
-                .append(" Phone: ")
-                .append(getPhone())
-                .append(" Email: ")
-                .append(getEmail())
-                .append(" Address: ")
-                .append(getAddress())
-                .append(" Tags: ");
-        getTags().forEach(builder::append);
+        builder.append(" Phone: ")
+                .append(getName());
+//                .append(" Phone: ")
+//                .append(getPhone())
+//                .append(" Email: ")
+//                .append(getEmail())
+//                .append(" Address: ")
+//                .append(getAddress())
+//                .append(" Tags: ");
+//        getTags().forEach(builder::append);
         return builder.toString();
     }
-
 }
