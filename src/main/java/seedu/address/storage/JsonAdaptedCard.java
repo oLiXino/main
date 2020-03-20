70ef3d2c66029fd508722e1d33af1a748b8ffa88
kschiew <kschiew98@gmail.com>
2020-03-20 14:03:30 +0800

    Enable storage function

---
+++ src/main/java/seedu/address/storage/JsonAdaptedCard.java
@@ -0,0 +1,69 @@
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.deck.Deck;
+import seedu.address.model.deck.Name;
+import seedu.address.model.deck.card.BackFace;
+import seedu.address.model.deck.card.Card;
+import seedu.address.model.deck.card.FrontFace;
+
+/**
+ * Jackson-friendly version of {@link Deck}.
+ */
+class JsonAdaptedCard {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Card name is missing!";
+
+    private final String frontFace;
+    private final String backFace;
+
+    /**
+     * Constructs a {@code JsonAdaptedDeck} with the given deck details.
+     */
+    @JsonCreator
+    public JsonAdaptedCard(@JsonProperty("frontFace") String frontFace, @JsonProperty("backFace") String backFace) {
+        this.frontFace = frontFace;
+        this.backFace = backFace;
+    }
+
+    /**
+     * Converts a given {@code Deck} into this class for Jackson use.
+     */
+    public JsonAdaptedCard(Card card) {
+        this.frontFace = card.getFrontFace().getValue();
+        this.backFace = card.getBackFace().getValue();
+    }
+
+    /**
+     * Getters for front face value.
+     */
+    public String getFrontFace() {
+        return frontFace;
+    }
+
+    /**
+     * Getters for Back face value.
+     */
+    public String getBackFace() {
+        return backFace;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted deck object into the model's {@code Deck} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted deck.
+     */
+    public Card toModelType() throws IllegalValueException {
+        if (frontFace == null || backFace == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT));
+        }
+
+        final FrontFace front = new FrontFace(frontFace);
+        final BackFace back = new BackFace(backFace);
+
+        return new Card(front, back);
+    }
+}
