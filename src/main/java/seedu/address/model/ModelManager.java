6da2c945b70ae86adbe2585a9f00f9d58931b74c
Robert Pham <robpham98@gmail.com>
2020-03-20 17:51:50 +0800

    play-return-card

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -319,6 +319,7 @@ public class ModelManager implements Model {
         }
         this.game = new GameManager(deck);
         this.mode = Mode.PLAY;
+        setCurrentMode(Mode.PLAY);
         return deck.asUnmodifiableObservableList().get(0);
     }
 
