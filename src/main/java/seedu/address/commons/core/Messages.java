a7c196dba39d1268ad6a2bbd5bcd896aa1c959e4
ncslzh <ncslzh@gmail.com>
2020-04-09 17:51:32 +0800

    Add exception thrown when user puts in more than 1 colon

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,6 +7,7 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command!";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INPUT_CONTAINS_COLON = "Card face value(s) should not contain colon(s)!";
 
     public static final String MESSAGE_INVALID_DECK_DISPLAYED_INDEX = "The deck index provided is invalid!";
     public static final String MESSAGE_INVALID_CARD_DISPLAYED_INDEX = "The card index provided is invalid!";
